c321409b3781355577b2d849128aa1842c2d4335
Soumya5122 <dhanappsoumya@gmail.com>
2022-05-25 05:23:02 +0530

    2

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,42 @@
 # Strings
 
 text = "X-DSPAM-Confidence:    0.8475"
+
+
+
+
+
+
+# Loops & Iterators
+
+largest = None
+smallest = None
+
+while True:
+    num = input("Enter a number? ")
+
+    if num == "done":
+        break
+
+    # ...
+
+    print(num)
+
+print("Maximum", largest)
+
+
+
+
+# Functions
+
+
+def computepay(h, r):
+    pass  # ...
+
+
+hrs = float(input("Enter hours? "))
+rte = float(input("Enter rate per hour? "))
+
+p = computepay(hrs, rte)
+print("Pay", p)
+
