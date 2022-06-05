171eeb3aa42c6e0c85b401731dbb9c51a5086651
Soumya5122 <dhanappsoumya@gmail.com>
2022-06-05 15:41:32 +0530

    8

---
+++ ActivitySet#1/problem#17.py
@@ -0,0 +1,23 @@
+
+
+from ctypes.wintypes import WORD
+from zipimport import zipimporter
+
+x="awesome"
+def my_function():
+    if x is'awesome':
+        print("python is "+x)
+        
+        try:
+          if x>"awesome":
+             print("python is worst")
+                
+        except:
+             print("done")
+          
+
+my_function()
+
+
+
+
