dea379af0f1b4f82e158a3a1174223ac0fa70b25
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2022-07-05 13:39:44 +0530

    Update problem#10.py

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,8 @@
-# Dictionaries
-
-filename = "dataset/mbox-short.txt"
+file = open("mbox-short.txt")
+count = 0
+for l in file:
+    if (l.startswith("From:")):
+        adr = l.split(" ")[1]
+        print(adr.rstrip())
+        count += 1
+print("There were", count, "lines in the file with From as the first word")
