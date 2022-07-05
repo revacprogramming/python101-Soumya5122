c3687b06a08171c135b3bfd0968cc729e40033f0
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2022-07-05 13:36:25 +0530

    Update problem#08.py

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,8 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+fh = open('romeo.txt','r')
+l = list()
+for line in fh:
+    line = line.split()
+    for word in line:
+        if word not in l:
+            l.append(word)
+print(sorted(l))
