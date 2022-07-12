ffdbcd6409c028c7e7c72041dd48bd9932c0c1aa
Soumya5122 <dhanappsoumya@gmail.com>
2022-07-12 10:26:43 +0530

    22

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -5,12 +5,4 @@ txt=text.find(":")
 Text=text[21:]
 final=float(Text)
 print(final)
-<<<<<<< HEAD
 
-
-
-
-
-
-=======
->>>>>>> e4187dd5339fc93afe8ac6bd398090d807787873
--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,15 +1,24 @@
-<<<<<<< HEAD
+
 # Files
 
 filename = open("dataset/mbox-short.txt", "r")
 print(filename.readline())
-=======
+
+fh=open('romeo.txt','a')
+fh.write("this is cool")
+fh.close()
+
 fh = open('romeo.txt','r')
 l = list()
 for line in fh:
-    line = line.split()
+    line = line.upper()
     for word in line:
         if word not in l:
             l.append(word)
 print(sorted(l))
->>>>>>> e4187dd5339fc93afe8ac6bd398090d807787873
+
+fh.close()
+
+fh=open('romeo.txt','r')
+fh.readline()
+fh.close
