ae6fc7f2e0566cdea710a83007e52d5065003a23
Soumya5122 <dhanappsoumya@gmail.com>
2022-07-12 14:43:46 +0530

    hj

---
+++ ActivitySet#1/practice1.py
@@ -0,0 +1,9 @@
+def func():
+   f=print("enter a file name:")
+   f.open()
+   if 'X' in f:
+     print(f.write("X-DSPAM-Confidence:  0.8475"))
+     print(f.read())
+
+def func():
+    print("invalid")
