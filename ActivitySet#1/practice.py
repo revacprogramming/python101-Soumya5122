eb154f8454b10144e7831ea139bb594b54c4a6aa
Soumya5122 <dhanappsoumya@gmail.com>
2022-07-12 19:23:23 +0530

    gh

--- ActivitySet#1/practice.py
+++ ActivitySet#1/practice.py
@@ -1,9 +1,14 @@
-f=open("romeo.txt","r")
-fh=f.read()
-lst=list()
-for line in fh:
-    line=line .split(":")
-    for word in fh:
-        if word not in lst:
-            print(lst.append(word))
-print(sorted(lst))
+count=0
+total=0
+file=open("romeo.txt",'r')
+
+for line in file:
+    count=count+1
+    x=line.find(':')
+    n=line[x+1:].strip()
+    value=float(n)
+    total=total+value
+    average=total/count
+    break
+print("Average:",average)
+print(file)
