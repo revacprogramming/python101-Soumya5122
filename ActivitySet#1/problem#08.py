69cf258c77f772420deeb6bc4bd73cbb22cfdfbc
Soumya5122 <dhanappsoumya@gmail.com>
2022-07-12 14:43:07 +0530

    bh

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -4,21 +4,12 @@
 filename = open("dataset/mbox-short.txt", "r")
 print(filename.readline())
 
-fh=open('romeo.txt','a')
-fh.write("this is cool")
-fh.close()
-
 fh = open('romeo.txt','r')
 l = list()
 for line in fh:
-    line = line.upper()
+    line = line.split()
     for word in line:
         if word not in l:
             l.append(word)
 print(sorted(l))
 
-fh.close()
-
-fh=open('romeo.txt','r')
-fh.readline()
-fh.close
