c95ba7855cf3f4653d34fbc7d30f7a46a92c4d21
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2022-07-24 09:18:20 +0530

    z

--- ActivitySet#1/problem#17.py
+++ ActivitySet#1/problem#17.py
@@ -1,21 +1,23 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
 
+url = input ('Enter url: ')
+print('Retrieving', url)
 
-x="awesome"
-def my_function():
-    if x is'awesome':
-        print("python is "+x)
-        
-        try:
-          if x>"awesome":
-             print("python is worst")
-                
-        except:
-             print("done")
-          
+total = 0
+count = 0
 
-my_function()     #it is fine if u don't have time for me, deep down i know you love me
-                      #variable
-                   
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print('Retrieved', len(data), 'characters')
 
+tree = ET.fromstring(data)
+lst = tree.findall ('comments/comment')
 
-
+for item in lst:
+    count = count + 1
+    t = item.find ('count').text
+    total = total + float (t)
+    
+print ('Count:', count)
+print ('Sum:' , total)
