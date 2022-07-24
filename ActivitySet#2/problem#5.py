5f7fdd7ecf743e4d32368bb047eb1997ccab198f
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2022-07-24 09:22:39 +0530

    ink

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -1,15 +1,29 @@
 
 
 def get_cs():
-    """get string input"""
+    a=(input("enter a string"))
+    return a
 
 
 def cs_to_dict(cs):
-    """convert connect string to a dictionary"""
+    a=cs.split(";")
+    j=list()
+    for i in a:
+        b=i.split("=")
+        j.append(b)
+    j.pop()
+    thisdict=dict()
+    for (i,j) in j:
+        thisdict.update({i:j})
+    return thisdict
+
 
 
 def dict_to_cs(d):
-    """convert a dictionary to connect string"""
+    k=str()
+    for i in d:
+        k=k+(i+"="+d[i]+";")
+    return k
 
 
 def main():
