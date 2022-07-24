33cbda952f2539fe762cce5669318a075e83b8e2
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2022-07-24 09:20:20 +0530

    ink

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,18 +1,17 @@
-
+def input_two_numbers():
+    a=int(input("enter the number"))
+    b=int(input("enter the number"))
+    return a,b
 def add(a, b):
-    pass  # ...
+    return a+b
 
 
 def output(a, b, sum):
-    pass  # ...
+    print("the sum of the two",a,b,"numbers is ",sum)
 
 
 def main():
     a, b = input_two_numbers()
     sum = add(a, b)
-
     output(a, b, sum)
-
-
-if __name__ == '__main__':
-    main()
+main()
