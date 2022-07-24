04c961ac8df6e4cb62936546cafe8dd6b74fead0
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2022-07-24 09:15:37 +0530

    z

--- ActivitySet#1/problem#20.py
+++ ActivitySet#1/problem#20.py
@@ -1,21 +1,29 @@
-count=1
-n1=0
-n2=1
-while True:
-    N=input('enter a number:')
-    n=int(N)
+#To be tested, not giving correct output.....
 
-    if n <= count:
-        n=n1+n2
-        n2=n1
-        n1=n
-        print(n1)
-        
-        continue
-    else:
 
-        print("I love you")
+import sqlite3
+conn = sqlite3.connect(':memory:')
+c = conn.cursor()
+c.execute("""CREATE TABLE Ages(
+             name VARCHAR(128),
+             age INTEGER
+             )"""
+         )
+def add_people(sName, iAge):
+    with conn:
+        c.execute("INSERT INTO Ages VALUES (:name, :age)", {'name':sName, 'age':iAge})
 
-        print("I love me")
+# dPeople = {'Oz': 25, 'Noor': 31, 'Praise': 22, 'Kerris': 26, 'Louise': 19, 'Ismaeel': 17 }
+# for keyName, valueAge in dPeople.items():
+#     add_people(keyName, valueAge)
 
-    
+lPeople = [('Oz', 25), ('Noor', 31), ('Praise', 22), ('Kerris', 26), ('Louise', 19), ('Ismaeel', 17)]
+for tItem in lPeople:
+    sName, iAge = tItem
+    add_people(sName, iAge)
+
+c.execute("SELECT hex(name || age) AS X FROM Ages ORDER BY X")
+print(c.fetchall())
+
+conn.commit()
+conn.close()
