cd686999c7ff5f99f403ad2ec4a864a8181667f8
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2022-07-24 09:19:38 +0530

    ink

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,9 @@
-
-
 def add(a, b):
-    return  # ...
-
-
+    c=a+b
+    return  c
 def main():
-    a = input  # ...
-    b = input  # ...
-
+    a = int(input("enter a number:"))
+    b = int(input("enter a number:"))
     c = add(a, b)
-    print  # ...
+    print("the sum of a and b is :",c)
+main()
