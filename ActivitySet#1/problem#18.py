1afe7ca4afad44b4fd2feeed8acc6c03d33eac88
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2022-07-24 09:17:39 +0530

    z

--- ActivitySet#1/problem#18.py
+++ ActivitySet#1/problem#18.py
@@ -1,8 +1,18 @@
-z="awesome"
-def variable_name():
-    global z
-    z="Lovely"
-    print("Pranjali is " +z)
+import urllib.request, json
 
-variable_name()
-print("Pranjali is " +z)
+address = input('Enter location: ')
+print('Retrieving', address)
+with urllib.request.urlopen(address) as url:
+    raw = json.loads(url.read().decode())
+
+print('Retrieved', len(str(raw)), 'characters')
+data = raw.get("comments")
+#print(data)
+num = total = 0
+for i in range(len(data)):
+    tmp = data[i]
+    value = tmp.get("count")
+    num = num + 1
+    total = total + int(value)
+print("Count:",num)
+print("Sum:",total)
